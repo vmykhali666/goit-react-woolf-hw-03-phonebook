95e5e08b5ff7b452d9f6035922c1a64acdb96564
Vladyslav Mykhalytskyi <vmykhali666@gmail.com>
2024-02-28 02:28:55 +0200

    local storage save added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,6 +31,19 @@ export class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   onRemoveContact = id => {
     this.setState(({ contacts }) => ({
       contacts: contacts.filter(contact => contact.id !== id),
